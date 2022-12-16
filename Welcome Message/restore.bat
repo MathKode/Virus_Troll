062224dec16e0b0e2a994008ee20c8e01902be70
BiMathAx <61063210+MathKode@users.noreply.github.com>
2022-12-16 12:04:02 +0100

    Delete Welcome Message directory

--- Welcome Message/restore.bat
+++
@@ -1,4 +0,0 @@
-@rem Delete The Welcome message
-@echo off
-
-del "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\welcome.bat"
--- Welcome Message/starter.bat
+++
@@ -1,4 +0,0 @@
-@rem Display a message each time you log in
-@echo off
-
-copy "Sample\general.bat"  "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\welcome.bat"
