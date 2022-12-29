0df5115f052c3c0f1e199c6aa739d51afbe881b1
BiMathAx <61063210+MathKode@users.noreply.github.com>
2022-12-29 13:09:56 +0100

    Update main.bat

--- Black_Screen/Code/main.bat
+++ Black_Screen/Code/main.bat
@@ -1,7 +1,7 @@
 @rem Fait un écran noire chaque TIME seconde
 @echo off
 title black_screen
-powershell (Get-WmiObject Win32_Process -Filter ProcessId=$PID).ParentProcessId > name.txt
+@rem powershell (Get-WmiObject Win32_Process -Filter ProcessId=$PID).ParentProcessId > name.txt
 set /a TIME=25
 :loop
 set PWS=powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile
